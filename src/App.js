32cfbcbad70ecc348f0601d31691f399a7d94083
Thiago Hardt <thiago.hardts@gmail.com>
2021-03-18 09:54:03 +0000

    Added basic structure of techs and logs.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
 
 function App() {
   useEffect(() => {
     //Initialize Materialze JS
     M.AutoInit();
   });
-  return <div className="App">My app</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        <Logs />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
