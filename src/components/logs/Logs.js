824b8028a4bbba543337635ce7039d73cafbacd8
Thiago Hardt <thiago.hardts@gmail.com>
2021-03-18 11:15:31 +0000

    Added delete, add and get functionality

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import LogItem from "./LogItem";
 import Preloader from "../layout/Preloader";
@@ -29,7 +29,10 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
   );
 };
 
-Logs.propTypes = { log: PropTypes.object.isRequired };
+Logs.propTypes = {
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = (state) => ({
   log: state.log,
